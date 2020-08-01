fb655156c6274fd4e0d01c8abf8c32aa5d296085
Rico <ricoedison@gmail.com>
2020-08-02 00:17:24 +0530

    new

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+$result="";
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -9,18 +10,30 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'ricoedisona@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@ricoedisonb@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+
+   $name = strip_tags(htmlspecialchars($_POST['name']));
+   $email_address = strip_tags(htmlspecialchars($_POST['email']));
+   $phone = strip_tags(htmlspecialchars($_POST['phone']));
+   $message = strip_tags(htmlspecialchars($_POST['message']));
+      
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+
+$mail->Host='smpt.gmail.com';
+$mail->Port=587;
+$mail->SMTPAuth=true;
+$mail->SMTPSecure='tls'
+$mail->Username='ricoedisonb@gmail.com';
+$mail->Passsword='01061997';
+
+$mail->setFrom($_POST['email'],$_POST['name']);
+$mail->addAddress('ricoedisona@gmail.com');
+
+
+$mail->isHTML(true);
+$mail_subject = "Website Contact Form:  $name";
+$mail_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$result= $_POST['name'];
+
 return true;         
 ?>
